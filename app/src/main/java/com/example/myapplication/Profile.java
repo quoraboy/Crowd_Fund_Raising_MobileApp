b6bcbe704e564b4eb49f9ee2e41c6eb0230695fc
quoraboy <sachin.s.pathak1998@gmail.com>
2019-08-23 16:12:40 +0530

    Profile section updated

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -8,14 +8,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Profile extends Fragment {
+   EditText name;
+   EditText age;
+   EditText organisation;
+   EditText qualification;
+   Button submit;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.rentit,container,false);
+        View v= inflater.inflate(R.layout.activity_profile,container,false);
+          name=v.findViewById(R.id.name);
+          age=v.findViewById(R.id.age);
+          organisation=v.findViewById(R.id.organisation);
+          qualification=v.findViewById(R.id.qualification);
+          submit=v.findViewById(R.id.Save);
+
+
+          submit.setText("Save Information");
+
+
     return  v;
     }
 }
