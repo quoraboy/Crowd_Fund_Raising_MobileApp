ac06747454678c305e0e96ef01f565817ab5a949
quoraboy <sachin.s.pathak1998@gmail.com>
2019-08-24 16:02:25 +0530

    Profile section done 95%

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,11 +23,11 @@ public class Profile extends Fragment {
    EditText organisation;
    EditText qualification;
    Button submit;
+   Button Edit;
    DatabaseReference mdatabase;
      SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-   String t="0";
-
+    String t;
     String phone;
     profileinfo profileinfo;
 
@@ -41,63 +42,60 @@ public class Profile extends Fragment {
           qualification=v.findViewById(R.id.qualification);
           submit=v.findViewById(R.id.Save);
           sharedPreferences = getActivity().getSharedPreferences("num", Context.MODE_PRIVATE);
-        phone=sharedPreferences.getString("Phone","121");     //Default value get displayed when no data is entered in main activity
+        Edit=v.findViewById(R.id.edit);
+          phone=sharedPreferences.getString("Phone","121");     //Default value get displayed when no data is entered in main activity
           mdatabase =FirebaseDatabase.getInstance().getReference();
           submit.setText("Save Information");
          sharedPreferences = getActivity().getSharedPreferences("profile", Context.MODE_PRIVATE);
-         t=sharedPreferences.getString("t","0");
        name.setText(sharedPreferences.getString("name",""));
        age.setText(sharedPreferences.getString("age",""));
        organisation.setText(sharedPreferences.getString("organisation",""));
        qualification.setText(sharedPreferences.getString("qualification",""));
-
-       submit.setOnClickListener(new View.OnClickListener() {
+       t=sharedPreferences.getString("t","0");
+       if(t.equals("1")) {
+            name.setEnabled(false);
+            age.setEnabled(false);
+            organisation.setEnabled(false);
+            qualification.setEnabled(false);
+        }
+         submit.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
 
-             if(t=="0") {
                  profileinfo = new profileinfo(name.getText().toString().trim(), age.getText().toString().trim(), qualification.getText().toString().trim(), organisation.getText().toString().trim());
                  mdatabase.child(phone).setValue(profileinfo);
-                 submit.setText("Edit Saved Information");
                  sharedPreferences = getActivity().getSharedPreferences("profile", Context.MODE_PRIVATE);
 
                  editor = sharedPreferences.edit();
-                 t = "1";
+                    t="1";
                  editor.putString("name", profileinfo.getName());
                  editor.putString("age", profileinfo.getAge());
                  editor.putString("organisation", profileinfo.getOrganization());
                  editor.putString("qualification", profileinfo.getQualification());
-                 editor.putString("t", t);
+                 editor.putString("t",t);
                  name.setEnabled(false);
                  age.setEnabled(false);
                  organisation.setEnabled(false);
                  qualification.setEnabled(false);
 
                  editor.commit();
-             }
-             else if(t=="1")
-             {
-
-                         name.setEnabled(true);
-                         age.setEnabled(true);
-                         organisation.setEnabled(true);
-                         qualification.setEnabled(true);
-                 sharedPreferences = getActivity().getSharedPreferences("profile", Context.MODE_PRIVATE);
-                 editor = sharedPreferences.edit();
-                         t="0";
-                 editor.putString("t", t);
-                 editor.commit();
-                         submit.setText("Saved Information");
-
-
-             }
            }
        });
 
 
+Edit.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        name.setEnabled(true);
+        age.setEnabled(true);
+        organisation.setEnabled(true);
+        qualification.setEnabled(true);
+
+    }
+});
 
 
 
-        return  v;
+         return  v;
     }
 }
